2038e90b14918d63ee2d90c7e48b840b60224595
Fernando Paniagua <fernando.paniagua.formacion@gmail.com>
2022-04-25 13:42:46 +0200

    First commit

---
+++ src/main/java/com/fernandopaniagua/demofsgserver/DemoFsgServerApplication.java
@@ -0,0 +1,13 @@
+package com.fernandopaniagua.demofsgserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFsgServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFsgServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fernandopaniagua/demofsgserver/DemoFsgServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernandopaniagua.demofsgserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFsgServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
